0623db815f9b0b82186e008a15143849b4285244
looa-glitch <65273021+looa-glitch@users.noreply.github.com>
2020-05-19 16:53:17 +0700

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,31 +1,14 @@
 var constant = {
     config: {
-        // csapi: "https://csapi.damcorp.id/api/v2",
-        // csapikey: "938382d6bbfc50d676b947678eb4cd8c536b2de82b3727568c0923d473e3521cc788e3b299b85e5bc33fd66234c4c16f0a3e989fbae78813e572e0df764fa1087c20c2e6088ae0b7c736fa6f01d3712e",
-        token: "token",
+token: "token",
         number: "number",
 	    whatsappApi: "https://waba.damcorp.id/",
         cms_uri: "http://localhost:6212",
         cms_token: "damcorp-123",
         port: "6112",
-        // lactogrow: {
-        //     base_url: "https://rewards.sahabatnestle.co.id/apps/snrv2/api/wa/inbound",
-        //     group_id_cs: "cY7f8YUWwDN0upuH274fL",
-    	//     group_id_expert: "3LckVcEFGQJC07_zc8pWq"
-        // },
+
         mongodb: "mongodb+srv://looa:0f3V8CakgSUFmK8h@research-0xpzd.gcp.mongodb.net/bot-puji?retryWrites=true&w=majority",
-        // cloudinary: { 
-        //     cloud_name: 'damcorp', 
-        //     api_key: '255341431681536', 
-        //     api_secret: 'AIw1jCjWq4wYILQK3VIZwuXrf9E' 
-        // },
-        // redis: {
-        //     client: {
-        //         host: "localhost",
-        //         port: 6379,
-        //         scheme: "1"
-        //     }
-        // }
+
     }
 }
 module.exports = constant
